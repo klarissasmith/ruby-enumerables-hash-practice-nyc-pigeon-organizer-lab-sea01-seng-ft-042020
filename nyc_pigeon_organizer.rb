a9ba409f32a4bf9317b2ec2cb46f9c548c91b2b3
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-10 02:59:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,12 +6,15 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         pigeons_by_names[name] ||= {}
         pigeons_by_names[name][characteristics_name] ||= []
+        # does this work because the left side returns false because it can't find pigeons_by_names[name] or pigeons_by_names[characteristics_name] so it completes the right side??
         pigeons_by_names[name][characteristics_name] << add_characteristics.to_s
       end
   end
 end
 pigeons_by_names
 end
+
+#found solution that uses pipes and equals. Why does this work but without it my ENTIRE values were missing and only the lives was working??
 
 # def nyc_pigeon_organizer(data)
 #   pigeon_list = {}
